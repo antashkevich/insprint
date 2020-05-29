6448a1f832697779b2f2685656bf02be6704ad79
Alex Antashkevich <antashkevich@vicman.net>
2020-05-29 20:29:10 +0300

    fixed size site

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -2,22 +2,22 @@
 //= ../../../node_modules/jquery/dist/jquery.js
 
 // Импортируем Popper
-//= ../../../node_modules/popper.js/dist/umd/popper.js
+// // = ../../../node_modules/popper.js/dist/umd/popper.js
 
 // Импортируем необходимые js-файлы Bootstrap 4
 //= ../../../node_modules/bootstrap/js/dist/util.js
-//= ../../../node_modules/bootstrap/js/dist/alert.js
+// //= ../../../node_modules/bootstrap/js/dist/alert.js
 //= ../../../node_modules/bootstrap/js/dist/button.js
-//= ../../../node_modules/bootstrap/js/dist/carousel.js
+// //= ../../../node_modules/bootstrap/js/dist/carousel.js
 //= ../../../node_modules/bootstrap/js/dist/collapse.js
 //= ../../../node_modules/bootstrap/js/dist/dropdown.js
-//= ../../../node_modules/bootstrap/js/dist/modal.js
-//= ../../../node_modules/bootstrap/js/dist/tooltip.js
-//= ../../../node_modules/bootstrap/js/dist/popover.js
-//= ../../../node_modules/bootstrap/js/dist/scrollspy.js
+// //= ../../../node_modules/bootstrap/js/dist/modal.js
+// //= ../../../node_modules/bootstrap/js/dist/tooltip.js
+// //= ../../../node_modules/bootstrap/js/dist/popover.js
+// //= ../../../node_modules/bootstrap/js/dist/scrollspy.js
 //= ../../../node_modules/bootstrap/js/dist/index.js
 //= ../../../node_modules/bootstrap/js/dist/tab.js
-//= ../../../node_modules/bootstrap/js/dist/toast.js
+// //= ../../../node_modules/bootstrap/js/dist/toast.js
 
 // Импортируем необходимые js-файлы Slick Slider
 //= ../../../node_modules/slick-carousel/slick/slick.min.js
